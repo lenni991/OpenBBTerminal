7aaf7e5acc9d19dcbdf1b52ad48260c4201a2f9e
martinb-bb <105685594+martinb-bb@users.noreply.github.com>
2023-02-17 11:09:34 -0500

    refactoring

--- openbb_terminal/data_aggregation/main_test.py
+++ openbb_terminal/data_aggregation/main_test.py
@@ -1,33 +1,25 @@
 from models.stockModel import StockDataModel
 from models.fundamentalModel import FundamentalDataModel
 
-API_POLYGON_KEY = "INSERT"
+requested_source = "polygon"
 
-stock_source = "polygon"
+if requested_source == "polygon":
+    API_KEY = "INSERT"
+elif requested_source == "yahoo":
+    API_KEY = None
 
 print("Testing stock data object")
 # create a stock object
 stockObject = StockDataModel()
-if stock_source == "polygon":
-    stockObject.load_from_api(
-        api_key=API_POLYGON_KEY,
-        source="polygon",
-        symbol="AAPL",
-        start_date="2023-01-01",
-        end_date="2023-01-09",
-        weekly=False,
-        monthly=False,
-    )
-elif stock_source == "yahoo":
-    stockObject.load_from_api(
-        api_key=API_POLYGON_KEY,
-        source="yahoo",
-        symbol="AAPL",
-        start_date="2023-01-01",
-        end_date="2023-01-09",
-        weekly=False,
-        monthly=False,
-    )
+stockObject.load_from_api(
+    api_key=API_KEY,
+    source=requested_source,
+    symbol="AAPL",
+    start_date="2023-01-01",
+    end_date="2023-01-09",
+    weekly=False,
+    monthly=False,
+)
 
 # Check data within object using its members
 if stockObject.verified:
@@ -38,7 +30,7 @@ if stockObject.verified:
 print("Testing fundamental data object")
 fundamentalObject = FundamentalDataModel()
 fundamentalObject.load_from_api(
-    api_key=API_POLYGON_KEY,
-    source="polygon",
+    api_key=API_KEY,
+    source=requested_source,
     symbol="AAPL",
 )
